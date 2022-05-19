69bd7afb811ba562fadbe19b4280c674fe968b25
codetrixter <Abhishek20893@gmail.com>
2022-05-19 10:03:49 +0530

    correcting insert at beg

--- Abhishek/linkedlists/displayCircularList.cpp
+++ Abhishek/linkedlists/displayCircularList.cpp
@@ -4,9 +4,9 @@
  * @brief creation and manipulation of circular list.
  * @version 0.1
  * @date 2022-05-17
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #include <stdio.h>
@@ -18,59 +18,69 @@ struct Node
 {
     int data;
     struct Node *next;
-}*first = NULL;
+} *first = NULL;
 
 void create(int *A, int size)
 {
     struct Node *t, *last;
-    first = (struct Node*)malloc(sizeof(struct Node));
+    first = (struct Node *)malloc(sizeof(struct Node));
     first->data = A[0];
     first->next = first;
     last = first;
-    for(int i = 1; i < size; i++)
+    for (int i = 1; i < size; i++)
     {
-        t = (struct Node*)malloc(sizeof(struct Node));
+        t = (struct Node *)malloc(sizeof(struct Node));
         t->data = A[i];
+        t->next = last->next;
         last->next = t;
-        t->next = first;
         last = t;
     }
 }
 
-void insertAtBeg(struct Node *p, int element)
+void insertAtBeg(struct Node *head, int element)
 {
-    struct Node *temp, *it;
-    temp = (struct Node*)malloc(sizeof(struct Node));
-    temp->data = element;
-    it = p;
+    struct Node *newNode, *it;
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = element;
+    newNode->next = head;
+    it = head;
 
-    while(it->next != p)
+    while (it->next != head)
     {
         it = it->next;
     }
-    it->next = temp;
+    it->next = newNode;
+    head = newNode;
+    //need to do this as head is a local copy of the pointer first
+    first = head;
+}
 
-    temp->next = p;
-    p = temp;
+void DisplayInC(struct Node *h)
+{
+    do
+    {
+        printf("%d ", h->data);
+        h = h->next;
+    } while (h != first);
 }
 
 void display(struct Node *start)
 {
     struct Node *temp = start;
-    while(temp->next != start)
+    while (temp->next != start)
     {
         std::cout << temp->data << " ";
         temp = temp->next;
     }
-    if(temp->next = start)
+    if (temp->next = start)
         std::cout << temp->data;
 
     std::cout << "\n";
 }
 
-struct Node* displayRecursive(struct Node * start)
+struct Node *displayRecursive(struct Node *start)
 {
-    if(start->next == first)
+    if (start->next == first)
     {
         std::cout << start->data;
         std::cout << "\n";
