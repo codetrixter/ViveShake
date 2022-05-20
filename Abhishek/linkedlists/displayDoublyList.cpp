22c31b6c96e8e257fa1754ddad8f5d95400f5bcf
codetrixter <Abhishek20893@gmail.com>
2022-05-21 00:21:55 +0530

    soubly linked list

---
+++ Abhishek/linkedlists/displayDoublyList.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file displayCircularList.cpp
+ * @author Abhishek
+ * @brief creation and manipulation of doubly list.
+ * @version 0.1
+ * @date 2022-05-17
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev;
+} *first = NULL;
+
+void create(int *A, int size)
+{
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    first->prev = NULL;
+    last = first;
+    for (int i = 1; i < size; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        t->prev = last;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *start)
+{
+    struct Node *temp = start;
+    while (temp != NULL)
+    {
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    std::cout << "\n";
+}
+
+void displayReverse(struct Node *start)
+{
+    struct Node *temp = start;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    while(temp != NULL)
+    {
+        std::cout << temp->data << " ";
+        temp = temp->prev;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int A[] = {10, 30, 2, 50, 1};
+    create(A, 5);
+    display(first);
+    displayReverse(first);
+    return 0;
+}
