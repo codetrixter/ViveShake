7a78c3f27267092c619c7138a84d5590a51baf2f
codetrixter <Abhishek20893@gmail.com>
2022-05-31 21:53:45 +0530

    oops concepts

---
+++ Abhishek/oops/memberInitList.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file memberInitList.cpp
+ * @author Abhishek Mishra      
+ * @brief C++11 provideas a way tp initialize the member variables of the class without asisgning values to them
+ * after they are created. It is called member initializer list.
+ * @version 0.1
+ * @date 2022-05-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+class A
+{
+    private:
+    int val_a;
+
+    public:
+    A(int val): val_a{val}
+    {
+        std::cout << "inside A ctor" << val_a << "\n";
+    }
+};
+
+class B
+{
+    private:
+    A mem_A;
+    const int Arr[5];
+    int sum;
+
+    public:
+    B(int valA, int valB):
+    mem_A{valA}, Arr{1, 2, 3, 4, 5}, sum{valB}      //initilaizing the A's class member variables and array
+    {
+        std::cout << "inside B ctor" << sum;
+    }
+
+};
+
+int main(int argc, char const *argv[])
+{
+    B nObj(5, 10);
+
+    return 0;
+}
---
+++ Abhishek/oops/nonstaticMembInit.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file nonstaticMembInit.cpp
+ * @author Abhishek 
+ * @brief non static member initializer list
+ * @version 0.1
+ * @date 2022-05-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+class square
+{
+    private:
+    int val_a{ 10 };
+    int val_b{ 20 };    //brace init
+    int val_d = 40;     //copy initi
+    //int val_c( 30 );        //parenthesis cannot be used for non static member init.
+
+    public:
+    square(int valA, int valB):
+    val_a{valA}, val_b{valB}
+    {
+        std::cout << "ctor" << val_a << val_b;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    square sq{10, 20};
+    square sqNew(30, 40);     
+    return 0;
+}
