613c2e748898bfeefe11ea1f7aded1d0804b0b92
vivyadav <vivek.yadav@healthedge.com>
2022-06-07 22:56:33 +0530

    Ugly no. 1

---
+++ Vivek/UglyNum1.java
@@ -0,0 +1,35 @@
+public class UglyNum1 {
+    public static void main(String[] args) {
+
+        //first 15 ugly no.s
+        for(int i = 1; i <= 15; i++)
+        System.out.print(getNthUglyNum(i)+", ");
+        
+    }
+
+    private static int getNthUglyNum(int n) {
+        int i = 1;
+        int count = 1;
+
+        while(n > count) {
+            i++;
+            if(isUglyNum(i)) {
+                count++;
+            }
+        }
+        return i;
+    }
+
+    private static boolean isUglyNum(int num) {
+        num = maxDivide(num, 2);
+        num = maxDivide(num, 3);
+        num = maxDivide(num, 5);
+        return num == 1;
+    }
+
+    private static int maxDivide(int num, int digit) {
+        while(num % digit == 0)
+            num = num/digit;
+        return num;
+    }
+}
