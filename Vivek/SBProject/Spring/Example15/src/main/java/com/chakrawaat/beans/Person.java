86ee6be21d3dac9c8fd6f2a1f144813c1ce1833d
vivyadav <vivek.yadav@healthedge.com>
2022-06-22 16:55:14 +0530

    Eager and Lazy instantiatoin of Singleton bean

--- Vivek/SBProject/Spring/Example15/src/main/java/com/chakrawaat/beans/Person.java
+++ Vivek/SBProject/Spring/Example15/src/main/java/com/chakrawaat/beans/Person.java
@@ -1,2 +1,26 @@
-package com.chakrawaat.beans;public class Person {
+package com.chakrawaat.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component(value="personBean")
+@Lazy
+public class Person {
+    private String name = "Vivek";
+    private final Vehicle vehicle;
+
+    @Autowired
+    public Person(Vehicle vehicle) {
+        System.out.println("Person bean is created by Spring Context!");
+        this.vehicle = vehicle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
