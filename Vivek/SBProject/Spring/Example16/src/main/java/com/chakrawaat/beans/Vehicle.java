19ac9d16682eadf64b33a915c5ea0bece1d3da05
vivyadav <vivek.yadav@healthedge.com>
2022-06-22 17:45:28 +0530

    Deepdive of Prototype Bean scope

---
+++ Vivek/SBProject/Spring/Example16/src/main/java/com/chakrawaat/beans/Vehicle.java
@@ -0,0 +1,25 @@
+package com.chakrawaat.beans;
+
+import com.chakrawaat.beans.services.VehicleServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+    private String name = "FZS";
+    private VehicleServices vehicleServices;
+
+    @Autowired
+    public Vehicle(VehicleServices vehicleServices) {
+        this.vehicleServices = vehicleServices;
+        System.out.println("Vehicle bean is created by Spring Framework!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- Vivek/SBProject/Spring/Example16/src/main/java/com/chakrawaat/main/Example16Main.java
+++ Vivek/SBProject/Spring/Example16/src/main/java/com/chakrawaat/main/Example16Main.java
@@ -1,19 +1,20 @@
 package com.chakrawaat.main;
 
-import com.chakrawaat.beans.VehicleServices;
+import com.chakrawaat.beans.services.VehicleServices;
 import com.chakrawaat.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Example14Main {
+public class Example16Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         var vehServices1 = context.getBean(VehicleServices.class);
         var vehServices2 = context.getBean("vehicleServices", VehicleServices.class);
+        System.out.println("vehServices1 hascode: "+vehServices1.hashCode());
+        System.out.println("vehServices2 hascode: "+vehServices2.hashCode());
 
-        System.out.println("vehServices1: hashcode() = "+vehServices1.hashCode());
-        System.out.println("vehServices2: hashcode() = "+vehServices2.hashCode());
-
-        if(vehServices1 == vehServices2)
-            System.out.println("Both beans of VehicleServices are same!");
+        if(vehServices1.hashCode() == vehServices2.hashCode())
+            System.out.println("Singleton Scope");
+        else
+            System.out.println("Prototype Scope");
     }
 }
