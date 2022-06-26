104ce447e00f6c5f9a7aac2baaca93485fc18178
codetrixter <Abhishek20893@gmail.com>
2022-06-27 00:08:31 +0530

    updated binary tree and ellipse-incomplete

---
+++ Abhishek/FunctionDynamics/ellipses.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file ellipses.cpp
+ * @author Abhishek
+ * @brief This feature lets you handle variable arguments(will do this later):
+ * 
+ * @version 0.1
+ * @date 2022-06-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+#include <cstdarg>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
