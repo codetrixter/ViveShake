7fe3e9e0f627f08c99422354c4bd89d761a09d3f
codetrixter <Abhishek20893@gmail.com>
2022-06-29 22:53:04 +0530

    lambda and list

--- Abhishek/FunctionDynamics/anonymousFunctions.cpp
+++ Abhishek/FunctionDynamics/anonymousFunctions.cpp
@@ -8,6 +8,10 @@
  * {
  *      function body
  * }
+ * 3- We can name/create variable for a lambda so that we can directly use that name 
+ * while passing as function parameter.
+ * 4- Lambda's can be stored using either auto(type deduction) or std::function.
+ * 5- use auto when initializing variable with lambda and use std::function otherwise.
  * @version 0.1
  * @date 2022-06-27
  * 
@@ -19,6 +23,7 @@
 #include <array>
 #include <string_view>
 #include <algorithm>
+#include <functional>
 
 // Our function will return true if the element matches
 // Here (C++17)std::string_view is the class which takes the copy of a string and then if another string is created from it
@@ -61,22 +66,77 @@ int main()
     return 0;
 } */
 
-//*************naming a lmabda***
-
+//*************naming a lambda***
 //naming the lambda so that it can be used easily without confusion.
 //instead of auto as a type inference, we can also use function pointer as long as the capture is empty.
-bool (*isEven)(int) {
-    [] (int n) {
-        return ((n%2) == 0);
-    }
-};
+// bool (*isEven)(int) {
+//     [] (int n) {
+//         return ((n%2) == 0);
+//     }
+// };
 
-int main(int argc, char const *argv[])
+// int main(int argc, char const *argv[])
+// {
+//     std::vector<int> array{ 2, 4, 6 };
+//     //std::all_of is a part of algorithm header, and it iterates through the given data structor and
+//     //checks for condition for each element of the structure
+//     std::cout << std::all_of(/* firs position interator */array.begin(), /* end pos iterator */array.end(), 
+//     /* pred function that accepts an element and return bool */ isEven);
+//     return 0;
+// }
+//*************naming a lambda***
+//*************storing lambda***
+/* int main(int argc, char const *argv[])
 {
-    std::vector<int> array{ 2, 4, 6 };
+    //this version cannot have captures
+    int (*addNumbers1)(int, int){
+        [](int a, int b)
+        {
+            return a+b;
+        }
+    };
 
-    std::cout << std::all_of(array.begin(), array.end(), isEven);
+    std::cout << addNumbers1(10, 20) << std::endl;
+
+    //This version can have captures as well
+    std::function<int(int,int)> addNumbers2{
+        [](int a, int b)
+        {
+            return a+b;
+        }
+    };
+
+    std::cout << addNumbers2(20, 30) << std::endl;
+
+    //This version of lambda can also have captures
+    auto addNumbers3{
+        [](int a, int b)
+        {
+            return a+b;
+        }
+    };
+
+    std::cout << addNumbers1(30, 40) << std::endl;
     return 0;
+} */
+//*************storing lambda***
+//*************using std::function for func param***
+// We don't know what fn will be. std::function works with regular functions and lambdas.
+//Here auto cannot be used, it is only allowed since c++20.
+void repeat(int repetitions, const std::function<void(int)>& fn)
+{
+  for (int i{ 0 }; i < repetitions; ++i)
+  {
+    fn(i);
+  }
 }
 
-//*************naming a lmabda***
+int main()
+{
+  repeat(3, [](int i) {
+    std::cout << i << '\n';
+  });
+
+  return 0;
+}
+//*************using std::function for func param***
