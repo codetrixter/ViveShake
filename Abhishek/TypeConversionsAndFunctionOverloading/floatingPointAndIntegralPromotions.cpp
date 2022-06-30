b0e594ae8c8792851adbe284912db20ffee2304a
codetrixter <Abhishek20893@gmail.com>
2022-06-30 22:29:52 +0530

    type conversions

---
+++ Abhishek/TypeConversionsAndFunctionOverloading/floatingPointAndIntegralPromotions.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file floatingPointAndIntegralPromotions.cpp
+ * @author Abhishek
+ * @brief Here we discuss the Numeric promotions:
+ * 1- This type of promotion a narrower numeric type is converted to a wider numeric type.
+ * 2- All numeric promotions are value preserving meaning there is no loss od data or precision.
+ * Numeric promotions are of two type:
+ * 1- Integral 
+ * 2- Floating point
+ * Integral Promotion:
+ *  - signed char or signed short can be converted to int.
+    - unsigned char, char8_t, and unsigned short can be converted to int if int can hold the entire range of the type, or unsigned int otherwise.
+    - If char is signed by default, it follows the signed char conversion rules above. If it is unsigned by default, it follows the unsigned char conversion rules above.
+    - bool can be converted to int, with false becoming 0 and true becoming 1.
+ * Floating Point promotion:
+    - value of type float can be converted to double.   
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+
+void printInt(int val)
+{
+    std::cout << val << std::endl;
+}
+
+void printDouble(double dd)
+{
+    std::cout << dd;
+}
+
+int main(int argc, char const *argv[])
+{
+    printInt(2);
+
+    //short is promoted to int
+    short s{3};
+    printInt(s);
+    //char is promoted to int
+    printInt('a');
+    //boolean is promoted to int
+    printInt(false);    
+
+    //float value is promoted to double
+    printDouble(3.5f);
+    
+    return 0;
+}
---
+++ Abhishek/TypeConversionsAndFunctionOverloading/implicitConversionORCoersion.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file implicitConversionORCoersion.cpp
+ * @author Abhishek 
+ * @brief Here we see the imliciti conversion dynamics:
+ * 1- Implicit type conversion cases.
+ * 2- C++ defines how different fundamental type are converted called as "standard conversions".
+ * These standard conversiona are divided into 4 categories:
+ * a- Numeric promotions.
+ * b- Numeric conversions.
+ * c- Arithmetic conversions.
+ * d- Other conversions.
+ * When a type conversion is needed, the compiler will see if there are standard conversion that it can 
+ * use to convert the value to the desired type. The compiler might ise 0 or more standard conversion in 
+ * the conversion process.
+ * @version 0.1
+ * @date 2022-06-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+//****************IMplicit type conversions***
+ 
+void doSomething(long ld)
+{}
+
+//Here integert type 7 is converted into float
+float foo()
+{
+    return 7;
+}
+
+int main(int argc, char const *argv[])
+{
+    //here integer type 3 is converted to type double.
+    double d{3};
+    d = 9;
+
+    //Here integer type 4 is converted to double
+    double division{3.0/4};
+
+    //Here integer type 5 is converted to boolean
+    if(5)
+
+    //Here passed integer value is converted to long
+    doSomething(5);
+
+    return 0;
+}
+//****************IMplicit type conversions***
