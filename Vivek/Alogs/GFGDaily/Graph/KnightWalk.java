2492ee91b960cd48e57c6af8460c0baf5ebb4d1e
vivyadav <vivek.yadav@healthedge.com>
2022-08-18 00:11:26 +0530

    Optimization of Knight Walk: Graph

--- Vivek/Alogs/GFGDaily/Graph/KnightWalk.java
+++ Vivek/Alogs/GFGDaily/Graph/KnightWalk.java
@@ -47,58 +47,63 @@ Constraints:
 public class KnightWalk {
     int currLayer = 0;
     int nextLayer = 0;
-    int [] dx = {1, 1, -1, -1, 2, -2, 2, -2};
-    int [] dy = {-2, 2, -2, 2, -1, -1, 1, 1};
-
+    int[] dx = { 1, 1, -1, -1, 2, -2, 2, -2 };
+    int[] dy = { -2, 2, -2, 2, -1, -1, 1, 1 };
+    boolean visited[][];
     int startXY, endXY;
-    public int minStepToReachTarget(int KnightPos[], int TargetPos[], int N)
-    {
+
+    public int minStepToReachTarget(int KnightPos[], int TargetPos[], int N) {
         // Code here
         startXY = 0;
-        endXY = N-1;
+        endXY = N - 1;
         int Kx = KnightPos[0] - 1, Ky = KnightPos[1] - 1;
-        int Tx = TargetPos[0] - 1,  Ty = TargetPos[1] - 1;
+        int Tx = TargetPos[0] - 1, Ty = TargetPos[1] - 1;
         int step = 0;
+        visited = new boolean[N][N];
         Queue<Integer> queX = new LinkedList<>();
         Queue<Integer> queY = new LinkedList<>();
         queX.add(Kx);
         queY.add(Ky);
+        visited[Kx][Ky] = true;
         currLayer = 1;
         nextLayer = 0;
+
         return getTarget(Tx, Ty, step, queX, queY);
     }
-    
+
     private int getTarget(int Tx, int Ty, int step, Queue<Integer> queX, Queue<Integer> queY) {
-        
-        while(!queX.isEmpty()) {
+
+        while (!queX.isEmpty()) {
             int Kx = queX.remove();
             int Ky = queY.remove();
             currLayer--;
-            if(Kx == Tx && Ky == Ty) return step;
-            
-            for(int i = 0; i< dx.length; i++) {
+            if (Kx == Tx && Ky == Ty)
+                return step;
+
+            for (int i = 0; i < dx.length; i++) {
                 int x = Kx + dx[i];
                 int y = Ky + dy[i];
-                
-                if(isInRange(x,y)) {
+
+                if (isInRange(x, y) && !visited[x][y]) {
                     queX.add(x);
                     queY.add(y);
+                    visited[x][y] = true;
                     nextLayer++;
                 }
             }
-            
-            if(currLayer == 0) {
+
+            if (currLayer == 0) {
                 step++;
                 currLayer = nextLayer;
-                nextLayer =0;
+                nextLayer = 0;
             }
-            
+
         }
-        
+
         return -1;
     }
-    
+
     private boolean isInRange(int x, int y) {
-        return x >= startXY && x <= endXY &&  y >= startXY && y <= endXY;
+        return x >= startXY && x <= endXY && y >= startXY && y <= endXY;
     }
 }
