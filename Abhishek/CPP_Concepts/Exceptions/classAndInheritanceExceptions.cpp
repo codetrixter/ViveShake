b73e0fdd306a930be921666f978a0609ea84aa65
codetrixter <Abhishek20893@gmail.com>
2022-09-12 21:55:16 +0530

    user defined excetion class

--- Abhishek/CPP_Concepts/Exceptions/classAndInheritanceExceptions.cpp
+++ Abhishek/CPP_Concepts/Exceptions/classAndInheritanceExceptions.cpp
@@ -4,6 +4,7 @@
  * @brief Here we discuss about the exceptional scenarios related to following:
  * 1- member functions: Exceptions can be used to indiacate error conditions that may occur  during the execution of out member function.
  * 2- Inheritance: 
+ * 3- User defined exceptions
  * @version 0.1
  * @date 2022-09-11
  * 
@@ -19,7 +20,8 @@
     return m_data[index];
 } */
 //*************In operator overloaded subscript***
-#include <iostream>
+//***************Excetion Inheritance***
+/* #include <iostream>
 
 class Member
 {
@@ -67,4 +69,58 @@ int main()
 	}
 
 	return 0;
+} */
+//***************Excetion Inheritance***
+//***************User defined Excetion***
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class ArrayException
+{
+private:
+	std::string m_error;
+
+public:
+	ArrayException(std::string_view error)
+		: m_error{ error }
+	{
+	}
+
+	const std::string& getError() const { return m_error; }
+};
+
+class IntArray
+{
+private:
+	int m_data[3]{}; // assume array is length 3 for simplicity
+
+public:
+	IntArray() {}
+
+	int getLength() const { return 3; }
+
+	int& operator[](const int index)
+	{
+		if (index < 0 || index >= getLength())
+			throw ArrayException{ "Invalid index" };
+
+		return m_data[index];
+	}
+
+};
+
+int main()
+{
+	IntArray array;
+
+	try
+	{
+		int value{ array[5] }; // out of range subscript
+	}
+	catch (const ArrayException& exception)
+	{
+		std::cerr << "An array exception occurred (" << exception.getError() << ")\n";
+	}
 }
+//***************User defined Excetion***
