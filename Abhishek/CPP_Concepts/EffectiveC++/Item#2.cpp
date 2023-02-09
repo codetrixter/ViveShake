b0a01f32d93ce80042d06804097e1a61c1542a75
codetrixter <abhishek20893@gmail.com>
2023-02-09 12:10:05 +0530

    leet coding

--- Abhishek/CPP_Concepts/EffectiveC++/Item#2.cpp
+++ Abhishek/CPP_Concepts/EffectiveC++/Item#2.cpp
@@ -1,3 +1,5 @@
 /**
  * *Prefer consts, enums and inlines to #defines.
+ * !1- For simple constants, prefer const objects or enums to #defines.
+ * !2- For function-like macros, prefer inline functions to #defines.
 */
