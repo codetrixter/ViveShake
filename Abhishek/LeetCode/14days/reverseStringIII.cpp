a40aa2bb2901be8376adfbe9969d169d7605aaf7
codetrixter <abhishek20893@gmail.com>
2023-02-13 23:53:01 +0530

    revering the string words at their place

---
+++ Abhishek/LeetCode/14days/reverseStringIII.cpp
@@ -0,0 +1,45 @@
+/**
+ * *Given a string s, reverse the order of characters in each word within a sentence while still
+ * preserving whitespace and initial word order.
+
+    Example 1:
+    Input: s = "Let's take LeetCode contest"
+    Output: "s'teL ekat edoCteeL tsetnoc"
+
+    Example 2:
+    Input: s = "God Ding"
+    Output: "doG gniD"
+
+    Constraints:
+    1 <= s.length <= 5 * 104
+    s contains printable ASCII characters.
+    s does not contain any leading or trailing spaces.
+    There is at least one word in s.
+    All the words in s are separated by a single space.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseWords(string s)
+{
+    stringstream stream(s);
+    string revStr = "";
+    string word = "";
+    while (stream >> word)
+    {
+        reverse(word.begin(), word.end());
+        revStr.append(word);
+        revStr += " ";
+    }
+    //removing the last space that was appended
+    revStr.erase(revStr.end()-1);
+    return revStr;
+}
+
+int main(int argc, char const *argv[])
+{
+    string input = "My name is Abhishek";
+    cout << reverseWords(input);
+    return 0;
+}
